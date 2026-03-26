d7b3a46b647ce6e90d74aaf2b9e498703f4e74da
Rahim10020 <rahialighi@gmail.com>
2026-03-26 23:22:16 +0000

    modifs

--- src/components/shared/LanguageSwitcher.tsx
+++ src/components/shared/LanguageSwitcher.tsx
@@ -2,10 +2,12 @@ import { CaretDownMdIcon, GlobeIcon } from "./icons";
 
 export default function LanguageSwitcher() {
   return (
-    <div className="flex items-center text-black-50">
+    <div className="flex items-center text-black-50 gap-2">
       <GlobeIcon size={18} />
-      <span className="text-sm font-normal ml-2">English(EN)</span>
-      <CaretDownMdIcon />
+      <div className="flex items-center cursor-pointer">
+        <span className="text-sm font-normal">English(EN)</span>
+        <CaretDownMdIcon />
+      </div>
     </div>
   );
 }
