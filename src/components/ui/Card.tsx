baeadc677c543a7de4fa6db4d0deb09d2fb5244d
Rahim10020 <rahialighi@gmail.com>
2026-04-21 15:51:25 +0000

    modifs

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -66,7 +66,7 @@ export function CardHeader({
  */
 export function CardTitle({ children, className = "" }: CardSubComponentProps) {
   return (
-    <h3 className={`text-2xl font-semibold text-foreground ${className}`}>
+    <h3 className={`text-2xl font-medium text-foreground ${className}`}>
       {children}
     </h3>
   );
