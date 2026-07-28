5a0f386c292d8fd41115a2ef604c14a363fcadfb
Rahim10020 <rahialighi@gmail.com>
2026-07-28 13:35:04 +0000

    add allowd origins in next config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ["192.168.1.87"],
   // Optimize images
   images: {
     formats: ['image/avif', 'image/webp'],
